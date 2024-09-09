6b5c4f451fb789c37cf44f09ee28a0b1017d45b3
mika@mikakreienbuehl.ch <mika@mikakreienbuehl.ch>
2024-09-09 21:48:58 +0200

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from app.models import User
 
 db = SQLAlchemy()
 jwt = JWTManager()
@@ -12,8 +13,7 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     
-    # MariaDB-Datenbank-Konfiguration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://<USERNAME>:<PASSWORD>@<SERVER>/<DATABASE>'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<USERNAME>:<PASSWORD>@<SERVER>/<DATABASE>'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'your-secret-key'
     app.config['SECRET_KEY'] = 'your-secret-key-for-sessions'
@@ -22,6 +22,11 @@ def create_app():
     jwt.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    
+    # Definiere, wie Flask-Login den Benutzer lädt
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
     
     from app.routes import main
     from app.api import api
